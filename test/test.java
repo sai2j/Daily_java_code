5122ccc7a8201782e69e7743953d3dcbbfbd5dad
Sahil Kolhe <sahilkoleh089@gmail.com>
2025-11-14 12:41:40 +0530

    add new folder

---
+++ test/test.java
@@ -0,0 +1,28 @@
+class Alpha
+{
+	public Alpha(String msg)
+	{
+		System.out.println("Alpha "+msg);
+	}
+}
+class Beta extends Alpha
+{
+	public Beta()
+	{
+		super(getMessage());
+		System.out.println("Beta class constructor!!");
+	}
+	
+	public static String getMessage()
+	{
+		return "class constructor";
+	}	
+}
+
+public class test
+{
+   public static void main(String[] argv) 
+   {
+	   new Beta();
+   }
+}
