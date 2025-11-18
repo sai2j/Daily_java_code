807f675511a0c87bb695e30cf7478341fffb08ef
Sai2j <sahiilkolhe089@gmail.com>
2025-11-18 10:19:31 +0530

    add

---
+++ static/staticdemo1.java
@@ -0,0 +1,17 @@
+
+class foo{
+          foo(){
+                    System.out.println("no argument constuctor");
+          }
+          {
+                    System.out.println("non statc block");
+          }
+          static{
+                    System.out.println("static block");
+          }
+}
+public class staticdemo1 {
+          public static void main(String[] args) {
+                    System.out.println("main method excecution");
+          }
+}
