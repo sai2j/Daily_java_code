7cd84ee8189ada4325111286bc4ba022992e3c41
sai2j <sahilkolhe089@gmail.com>
2025-11-23 21:22:04 +0530

    some change

--- inheritance2/StudentTracker.java
+++ inheritance2/StudentTracker.java
@@ -18,30 +18,33 @@ public class StudentTracker {
                     System.out.println("lAttendance");
                     double labAttandance = Double.parseDouble(sc.nextLine());
 
-                   System.out.println("=== Placement Evaluation Menu ==="); 
-                   System.out.println("1. View All Scores ");
-                   System.out.println("2. Check Placement Probability");
-                   System.out.println("3. Check Placement Eligibility");
-                   System.out.println("4. Exit"); 
-                   PlacementCandidate p1 = new PlacementCandidate(name, id, hardWorkScore, taskcomplete, classAttendance, labAttandance);
-                   boolean condition = true;
-                   while (condition) {
-                   int choice = sc.nextInt();
-                   switch(choice){
-                    case 1->{
-                              p1.evaluateCandidate();
+                    System.out.println("=== Placement Evaluation Menu ===");
+                    System.out.println("1. View All Scores ");
+                    System.out.println("2. Check Placement Probability");
+                    System.out.println("3. Check Placement Eligibility");
+                    System.out.println("4. Exit");
+                    PlacementCandidate p1 = new PlacementCandidate(name, id, hardWorkScore, taskcomplete,
+                                        classAttendance, labAttandance);
+                    boolean condition = true;
+                    while (condition) {
+                              int choice = sc.nextInt();
+                              switch (choice) {
+                                        case 1 -> {
+                                                  p1.evaluateCandidate();
+                                        }
+                                        case 2 -> {
+                                                  p1.probility();
+                                        }
+                                        case 3 -> {
+                                                  p1.Eligiblity();
+                                        }
+                                        case 4 -> {
+                                                  System.out.println("exit the code");
+                                                  System.exit(0);
+                                        }
+
+                              }
                     }
-                    case 2->{
-                              p1.probility();
-                    }
-                    case 3->{
-                              p1.Eligiblity();
-                    }
-                    case 4->{
-                              System.out.println("exit the code");
-                              System.exit(0);
-                    }
-                   }
-          }
+                    sc.close();
           }
 }
