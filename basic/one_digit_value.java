3822a099cc3fd47aa1483d372b41b2ef0f80b4a1
sai2j <sahilkolhe089@gmail.com>
2025-11-24 23:25:53 +0530

    modifiied code

--- basic/one_digit_value.java
+++ basic/one_digit_value.java
@@ -3,8 +3,7 @@ package basic;
 public class one_digit_value {
           public static void main(String[] args) {
                     int num = 999;
-
-                    for (int i = 0; num > 9; i++) {
+                    for (; num > 9;) {
                               int sum = 0;
                               for (int j = num; j != 0; j /= 10) {
                                         sum = sum + (j % 10);
