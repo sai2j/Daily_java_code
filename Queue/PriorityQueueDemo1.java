9385a80a0aa4edccd3f7496341e283dcab75c348
sai2j <sahilkolhe089@gmail.com>
2026-02-08 23:34:51 +0530

    priority queue

---
+++ Queue/PriorityQueueDemo1.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo1 {
+          public static void main(String[] args) {
+                    PriorityQueue<Object> pq = new PriorityQueue<>();
+                    pq.add("Orange");
+                    pq.add("Apple");
+                    pq.add("Mango");
+                    pq.add("Gaurav");
+                    pq.add("Grapes");
+
+                    System.out.println(pq);
+          }
+}
